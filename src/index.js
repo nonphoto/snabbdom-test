ea37a2a87d1083e886d32742ef2352625db1e531
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-06-07 14:26:36 -0400

    Initial snabbdom setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { h, init } from 'snabbdom';
+import classModule from 'snabbdom/modules/class';
+import propsModule from 'snabbdom/modules/props';
+import styleModule from 'snabbdom/modules/style';
+import eventModule from 'snabbdom/modules/eventlisteners';
 
-import confetti from 'canvas-confetti';
+const patch = init([classModule, propsModule, styleModule, eventModule]);
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+console.log(patch);
+
+var container = document.getElementById('root');
+
+var vnode = h(
+  'div#container.two.classes',
+  { on: { click: () => void console.log('hello') } },
+  [
+    h('span', { style: { fontWeight: 'bold' } }, 'This is bold'),
+    ' and this is just normal text',
+    h('a', { props: { href: '/foo' } }, "I'll take you places!"),
+  ],
+);
+// Patch into empty DOM element – this modifies the DOM as a side effect
+patch(container, vnode);
+
+var newVnode = h(
+  'div#container.two.classes',
+  { on: { click: () => void console.log('hello') } },
+  [
+    h(
+      'span',
+      { style: { fontWeight: 'normal', fontStyle: 'italic' } },
+      'This is now italic type',
+    ),
+    ' and this is still just normal text',
+    h('a', { props: { href: '/bar' } }, "I'll take you places!"),
+  ],
+);
+// Second `patch` invocation
+patch(vnode, newVnode); // Snabbdom efficiently updates the old view to the new state
