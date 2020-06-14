a42ef34fca4c4fec8cd9b546b0e8b487b9cf01fa
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-06-13 23:35:52 -0400

    Load index.js with import map

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import { h, init } from 'snabbdom';
-import classModule from 'snabbdom/modules/class';
-import propsModule from 'snabbdom/modules/props';
-import styleModule from 'snabbdom/modules/style';
-import eventModule from 'snabbdom/modules/eventlisteners';
+import { h, init } from "snabbdom/snabbdom";
+import classModule from "snabbdom/modules/class";
+import propsModule from "snabbdom/modules/props";
+import styleModule from "snabbdom/modules/style";
+import eventModule from "snabbdom/modules/eventlisteners";
 
 const patch = init([classModule, propsModule, styleModule, eventModule]);
 
 console.log(patch);
 
-var container = document.getElementById('root');
+var container = document.getElementById("root");
 
 var vnode = h(
-  'div#container.two.classes',
-  { on: { click: () => void console.log('hello') } },
+  "div#container.two.classes",
+  { on: { click: () => void console.log("hello") } },
   [
-    h('span', { style: { fontWeight: 'bold' } }, 'This is bold'),
-    ' and this is just normal text',
-    h('a', { props: { href: '/foo' } }, "I'll take you places!"),
-  ],
+    h("span", { style: { fontWeight: "bold" } }, "This is bold"),
+    " and this is just normal text",
+    h("a", { props: { href: "/foo" } }, "I'll take you places!"),
+  ]
 );
 // Patch into empty DOM element – this modifies the DOM as a side effect
 patch(container, vnode);
 
 var newVnode = h(
-  'div#container.two.classes',
-  { on: { click: () => void console.log('hello') } },
+  "div#container.two.classes",
+  { on: { click: () => void console.log("hello") } },
   [
     h(
-      'span',
-      { style: { fontWeight: 'normal', fontStyle: 'italic' } },
-      'This is now italic type',
+      "span",
+      { style: { fontWeight: "normal", fontStyle: "italic" } },
+      "This is now italic type"
     ),
-    ' and this is still just normal text',
-    h('a', { props: { href: '/bar' } }, "I'll take you places!"),
-  ],
+    " and this is still just normal text",
+    h("a", { props: { href: "/bar" } }, "I'll take you places!"),
+  ]
 );
 // Second `patch` invocation
 patch(vnode, newVnode); // Snabbdom efficiently updates the old view to the new state
