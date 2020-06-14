051c758800d86db790c075c2b1114c841ccf7978
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-06-14 03:54:17 -0400

    Color palette test

--- src/index.js
+++ src/index.js
@@ -4,36 +4,27 @@ import propsModule from "snabbdom/modules/props";
 import styleModule from "snabbdom/modules/style";
 import eventModule from "snabbdom/modules/eventlisteners";
 
+import { range } from "lodash";
+import { lch } from "d3-color";
+
+const count = 8;
+const swatches = range(count).map((n) => {
+  const color = lch(78, 33, (n * 360) / count).toString();
+  return h("div", {
+    style: {
+      width: "4rem",
+      height: "4rem",
+      background: color,
+      borderRadius: "100vw",
+      marginLeft: "-1rem",
+      border: "solid 0.2rem white",
+    },
+  });
+});
+
 const patch = init([classModule, propsModule, styleModule, eventModule]);
 
-console.log(patch);
-
-var container = document.getElementById("root");
-
-var vnode = h(
-  "div#container.two.classes",
-  { on: { click: () => void console.log("hello") } },
-  [
-    h("span", { style: { fontWeight: "bold" } }, "This is bold"),
-    " and this is just normal text",
-    h("a", { props: { href: "/foo" } }, "I'll take you places!"),
-  ]
+patch(
+  document.getElementById("root"),
+  h("div", { style: { margin: "1rem", display: "flex" } }, swatches)
 );
-// Patch into empty DOM element – this modifies the DOM as a side effect
-patch(container, vnode);
-
-var newVnode = h(
-  "div#container.two.classes",
-  { on: { click: () => void console.log("hello") } },
-  [
-    h(
-      "span",
-      { style: { fontWeight: "normal", fontStyle: "italic" } },
-      "This is now italic type"
-    ),
-    " and this is still just normal text",
-    h("a", { props: { href: "/bar" } }, "I'll take you places!"),
-  ]
-);
-// Second `patch` invocation
-patch(vnode, newVnode); // Snabbdom efficiently updates the old view to the new state
