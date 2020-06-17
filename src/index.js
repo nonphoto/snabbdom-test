d89f8dd8988c6daa5b5f212d1ab42529e9ba3fea
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-06-16 22:51:40 -0400

    Persistent state

--- src/index.js
+++ src/index.js
@@ -4,90 +4,104 @@ import propsModule from "snabbdom/modules/props";
 import styleModule from "snabbdom/modules/style";
 import eventModule from "snabbdom/modules/eventlisteners";
 import toVNode from "snabbdom/tovnode";
-import { range, merge } from "lodash";
+import { range } from "lodash";
 import { lch } from "d3-color";
 
-const patch = init([classModule, propsModule, styleModule, eventModule]);
+const patch = init([
+  classModule,
+  propsModule,
+  styleModule,
+  eventModule,
+  {
+    create: (_, vnode) => {
+      if (!vnode.write) {
+        const key = Symbol(vnode.key);
+        vnode.write = (value) => {
+          state[key] = value;
+        };
+      }
+    },
+  },
+]);
 
 let vnode = toVNode(document.getElementById("root"));
 
-const render = (f) => {
-  const next = f();
+const render = () => {
+  const next = main();
+  console.log("main", next);
   patch(vnode, next);
   vnode = next;
 };
 
-const element = (selector) => (...args) => {
-  const props = [];
-  const children = [];
-
-  for (let arg of args) {
-    if (typeof arg === "object") {
-      if (typeof arg.sel === "string") {
-        children.push(arg);
-      } else {
-        props.push(arg);
-      }
-    }
-
-    if (typeof arg === "string" || typeof arg === "number") {
-      children.push(arg.toString());
-    }
-  }
-
-  return h(selector, merge(...props), children);
+const element = (selector) => (data, children) => {
+  return h(
+    selector,
+    data,
+    typeof children === "function" ? children(0, () => {}) : children
+  );
 };
 
 element.div = element("div");
 
-let state = {};
+const createWrite = (node) => (value) => {
+  node.data.state = value;
+  render();
+};
 
-const store = (key, initialState, callback) => {
-  return [
-    {
-      hook: {
-        init: ({ key }) => {
-          state[key] = initialState;
-        },
-        destroy: ({ key }) => {
-          delete state[key];
-        },
-      },
-    },
-    ...callback(state[key] || initialState, (next) => {
-      state[key] = next;
-      render(main);
-    }),
-  ];
+const read = (initialState, fn) => {
+  const init = (node) => {
+    node.children = node.data.fn(initialState, createWrite(node));
+    node.data.state = initialState;
+  };
+
+  const prepatch = (prev, next) => {
+    next.children = next.data.fn(prev.data.state, createWrite(next));
+    next.data.state = prev.data.state;
+  };
+
+  return { fn, hook: { init, prepatch } };
 };
 
 const main = () => {
   const count = 8;
+
   const swatches = range(count).map((n) => {
     const color = lch(78, 33, (n * 360) / count).toString();
-    return element.div(
-      {
-        key: n,
-        style: {
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontFamily: "sans-serif",
-          background: color,
-          borderRadius: "100vw",
-          border: "solid 0.2rem white",
-          marginLeft: "-1rem",
-          padding: "1rem 2rem",
-          userSelect: "none",
-        },
+    return element.div({
+      key: n,
+      style: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "sans-serif",
+        background: color,
+        borderRadius: "100vw",
+        border: "solid 0.2rem white",
+        marginLeft: "-1rem",
+        padding: "1rem 2rem",
+        userSelect: "none",
       },
-      ...store(n, 0, (v, set) => [{ on: { click: () => set(v + 1) } }, v])
-    );
+      ...read(0, (value, write) => [
+        element.div(
+          {
+            on: {
+              click: () => {
+                write(value + 1);
+              },
+            },
+          },
+          value.toString()
+        ),
+      ]),
+    });
   });
 
-  return element.div(...swatches, {
-    style: { display: "flex", margin: "1rem" },
-  });
+  return element.div(
+    {
+      style: { display: "flex", margin: "1rem" },
+    },
+    swatches
+  );
 };
 
-render(main);
+render();
