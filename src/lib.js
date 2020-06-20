9e3eed376b41b794c0b183c472a63291775f5b93
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-06-20 12:30:44 -0400

    Create element shortcut for all tag names

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,4 @@
+import tagNames from "tag-names";
 import { h, init as snabbdom } from "snabbdom/snabbdom";
 import classModule from "snabbdom/modules/class";
 import propsModule from "snabbdom/modules/props";
@@ -25,29 +26,27 @@ const render = () => {
 };
 
 export const element = (selector) => (data, children) => {
-  return h(
-    selector,
-    data,
-    typeof children === "function" ? children(0, () => {}) : children
-  );
+  return h(selector, data, children);
 };
 
-element.div = element("div");
-element.button = element("button");
-
-const createWrite = (node) => (value) => {
-  node.data.state = value;
-  render();
-};
+for (let tag of tagNames) {
+  element[tag] = element(tag);
+}
 
 export const read = (data, getChildren) => {
   const init = (node) => {
-    node.children = node.data.getChildren(node.data.initial, createWrite(node));
+    node.children = node.data.getChildren(node.data.initial, (value) => {
+      node.data.state = value;
+      render();
+    });
     node.data.state = node.data.initial;
   };
 
   const prepatch = (prev, next) => {
-    next.children = next.data.getChildren(prev.data.state, createWrite(next));
+    next.children = next.data.getChildren(prev.data.state, (value) => {
+      next.data.state = value;
+      render();
+    });
     next.data.state = prev.data.state;
   };
 
